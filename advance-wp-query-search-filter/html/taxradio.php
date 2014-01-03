259dfeff7da6d6e9c93f749c929ac3026df25692
itivae <Noah@theredkeep.com>
2014-01-03 14:58:06 -0500

    Added <li> element to the listed taxonomy in taxradio.php. Added dropdown image. removed the default search all label.

--- advance-wp-query-search-filter/html/taxradio.php
+++ advance-wp-query-search-filter/html/taxradio.php
@@ -1,16 +1,30 @@
+
 <?php
+
+
 if ( $count > 0 ){
-	echo '<div class="awqsf_box taxoradio-'.$c.'"><label class="taxo-label-'.$c.'">'.$v['taxlabel'].'</label><br>';
+			
+			
+			echo '<li class="searchl">';
+			echo '<h3 class="search-list">';
+	echo '<div class="awqsf_box taxoradio-'.$c.'"><label class="taxo-label-'.$c.'">'.$v['taxlabel'].'</label>&nbsp;&nbsp;<img src="/wp-content/themes/responsive-child-SRI/images/down.png" alt="down arrow"/><br>';
+		echo '</h3>';
+		echo '<div>';
 	echo '<input  type="hidden" name="taxo['.$c.'][name]" value="'.$v['taxname'].'">';
-	echo '<label class="taxradio"><input type="radio" id="taxo-'.$c.'" name="taxo['.$c.'][term]" value="wqsftaxoall" ',(isset($_GET['taxo'][$c]['term']) &&  $_GET['taxo'][$c]['term']=='wqsftaxoall') ? 'checked="checked"' : '','>'.$v['taxall'].'</label>';
+	//echo '<label class="taxradio"><input type="radio" id="taxo-'.$c.'" name="taxo['.$c.'][term]" value="wqsftaxoall" ',(isset($_GET['taxo'][$c]['term']) &&  $_GET['taxo'][$c]['term']=='wqsftaxoall') ? 'checked="checked"' : '','>'.$v['taxall'].'</label>';
+	
 	foreach ( $terms as $term ) {
 		$selected = '';
 		
 		$selected = (isset($_GET['taxo'][$c]['term']) && $_GET['taxo'][$c]['term']==$term->slug) ? 'checked="checked"' : '';
 		echo '<label class="taxradio"><input type="radio" id="taxo-'.$c.'" name="taxo['.$c.'][term]" value="'.$term->slug.'" '.$selected.'>'.$term->name.'</label>';
-	}
 
-echo '<br><div style="clear:both"></div>';
+	}
 echo '</div>';
+echo '</li>';
+echo '<div style="clear:both"></div>';
+
+
+
 }
 ?>
